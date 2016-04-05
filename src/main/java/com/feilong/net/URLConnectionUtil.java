d3a34dd861988eb61d8a8c9fde6ef48df8e5c541
feilong <venusdrogon@163.com>
2016-04-05 22:43:16 +0800

    update javadoc

--- src/main/java/com/feilong/net/URLConnectionUtil.java
+++ src/main/java/com/feilong/net/URLConnectionUtil.java
@@ -340,7 +340,7 @@ public final class URLConnectionUtil{
         httpURLConnection.setDoOutput(HttpMethodType.POST == httpMethodType);
 
         //**********************************************
-        //设置默认的UA， 你可以使用headerMap来覆盖
+        //设置默认的UA, 你可以使用headerMap来覆盖
         httpURLConnection.setRequestProperty("User-Agent", HttpRequest.DEFAULT_USER_AGENT);
         //**********************************************
 
