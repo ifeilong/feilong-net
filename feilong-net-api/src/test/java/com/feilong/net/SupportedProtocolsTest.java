29d6fed1f91f6810b8c6d234eafe937d51327ba4
feilong <venusdrogon@163.com>
2018-04-09 22:02:00 +0800

    :memo: update javadoc

--- feilong-net-api/src/test/java/com/feilong/net/SupportedProtocolsTest.java
+++ feilong-net-api/src/test/java/com/feilong/net/SupportedProtocolsTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package snippet;
+package com.feilong.net;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
