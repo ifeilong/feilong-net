1fada48aa99ad2db8b845f98c7bd67cb9d62808c
feilong <venusdrogon@163.com>
2020-03-23 18:49:59 +0800

    :white_check_mark: update test

--- feilong-net-httpclient4/src/test/java/com/feilong/temp/HttpClientCodeTest.java
+++ feilong-net-httpclient4/src/test/java/com/feilong/temp/HttpClientCodeTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.feilong.test;
+package com.feilong.temp;
 
 import static org.junit.Assert.assertEquals;
 
