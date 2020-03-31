ee01777d2d1d4b37f33d3c27cf52f2bac3f00a7c
feilong <venusdrogon@163.com>
2020-03-31 17:10:12 +0800

    添加私有构造函数

--- feilong-net-mail/src/main/java/com/feilong/net/mail/builer/MessageBuilder.java
+++ feilong-net-mail/src/main/java/com/feilong/net/mail/builer/MessageBuilder.java
@@ -38,6 +38,15 @@ import com.feilong.tools.slf4j.Slf4jUtil;
  */
 public class MessageBuilder{
 
+    /** Don't let anyone instantiate this class. */
+    private MessageBuilder(){
+        //AssertionError不是必须的. 但它可以避免不小心在类的内部调用构造器. 保证该类在任何情况下都不会被实例化.
+        //see 《Effective Java》 2nd
+        throw new AssertionError("No " + getClass().getName() + " instances for you!");
+    }
+
+    //---------------------------------------------------------------
+
     /**
      * 构造Message.
      * 
